e73446021571e5891add345b580a343b0dc506c2
Sam Domesjö <samdomesjo@gmail.com>
2017-06-23 09:35:02 +0100

    angry

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,5 +1,8 @@
 class Bob
   def self.hey(str)
+    if str[str.length-1] == '?'
+       return response('question')
+    end
     return response('none') unless str.upcase == str
     return response('angry')
   end
