c537d9a18cb3f9e35330b2c1278488093795aa02
Sam Domesjö <samdomesjo@gmail.com>
2017-06-27 21:32:17 +0100

    bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,11 +1,14 @@
 class Bob
   def self.hey(str)
-    if str[str.length-1] == '?' && !(str == str.upcase)
+
+    if str[str.length-1] == '?' && !(str == str.upcase) || str.count("a-zA-Z") <= 0 && str[str.length-1] == '?'
        return response('question')
-    elsif str == str.upcase && str.include?(/[A-Za-z]/)
+    elsif str == ''
+      return response('empty')
+    elsif str == str.upcase && str.count("a-zA-Z") > 0
       return response('angry')
     end
-    return response('none') unless str.upcase == str
+    return response('none') unless str == str.upcase && str.count("a-zA-Z") > 0
     return response('angry')
   end
   def self.response(i)
