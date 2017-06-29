9bcfe875e84d252ed16b695e85b2c2a252e726a3
Sam Domesjö <samdomesjo@gmail.com>
2017-06-29 23:28:20 +0100

    encode

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,7 +1,22 @@
 class RunLengthEncoding
-  def self.encode(input)
-    ans = input
 
-    return ans
-  end
+    def self.encode(input)
+      arr = input.chars
+      ans = ''
+      count = 1
+      arr.each_index do |index |
+        if arr[index] != arr[index+1]
+          if count > 1
+            ans += count.to_s + arr[index]
+          else
+            ans += arr[index]
+          end
+            count = 1
+        else
+          count += 1
+        end
+      end
+      return ans
+    end
+
 end
