113051beb66d77f6d5bba6e9a7d0811bb2ba08c4
Sam Domesjö <samdomesjo@gmail.com>
2017-06-29 16:11:39 +0100

    trying to impkement recursion

--- ruby/run-length-encoding/run_length_encoding_test.rb
+++ ruby/run-length-encoding/run_length_encoding_test.rb
@@ -12,7 +12,7 @@ class RunLengthEncodingTest < Minitest::Test
   end
 
   def test_encode_single_characters_only_are_encoded_without_count
-    skip
+    
     input = 'XYZ'
     output = 'XYZ'
     assert_equal output, RunLengthEncoding.encode(input)
