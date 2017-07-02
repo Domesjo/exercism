13de262e145b01eca85cb26dedc68cc25818d34f
Sam Domesjö <samdomesjo@gmail.com>
2017-07-02 23:04:12 +0100

    done

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,15 +1,22 @@
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
 class Binary
   # binary multipliced (n*2)^2
   def self.to_decimal(n)
-    decimal = 0
     for i in n.chars
-      index = n.chars.index i
-      decimal += converter(i.to_i, index)
+      raise ArgumentError.new("Not valid binary") if i.match(/[a-zA-Z2-9]/)
     end
-    return decimal.to_i
+    decimal = 0
+    n.chars.each.with_index do |i, index|
+      decimal += converter(i.to_i, ((n.chars.length - 1 ) - index))
+    end
+    return decimal
   end
   def self.converter(n, index)
-    return (n*2)**index unless n== 0
+    return ((n*2)**index) unless n == 0
     0
   end
 end
+
+Binary.to_decimal('11')
