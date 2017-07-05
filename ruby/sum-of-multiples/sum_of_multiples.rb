6bb2abe43c1b9579977cb36895b0ccdac634b269
Sam Domesjö <samdomesjo@gmail.com>
2017-07-05 21:42:42 +0100

    making all option optional

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,5 +1,5 @@
 class SumOfMultiples
-  def initialize(n1, n2, n3=nil)
+  def initialize(n1=nil, n2=nil, n3=nil)
     @n1 = n1
     @n2 = n2
     @n3 = n3
@@ -8,9 +8,9 @@ class SumOfMultiples
   def to(int)
     ans = 0
     (1..int-1).to_a.each do |number|
-      if number % @n1 == 0
+      if @n1 && number % @n1 == 0
         ans += number
-      elsif number % @n2 == 0
+      elsif @n2 && number % @n2 == 0
         ans += number
       elsif @n3 && number % @n3 == 0
         ans += number
