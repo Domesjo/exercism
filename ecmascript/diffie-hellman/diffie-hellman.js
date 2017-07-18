ec0fbad69d374caa80aa9afdaa180a843a7720ae
Sam Domesjö <samdomesjo@gmail.com>
2017-07-18 11:48:49 +0100

    on the board

---
+++ ecmascript/diffie-hellman/diffie-hellman.js
@@ -0,0 +1,17 @@
+export default class DiffieHellman { // Takes two arguments p and g
+  constructor(p, g) { // p has to be  p > 1 => TRUE && both has to be a prime number
+    if (p > 1 && this.isPrime(p) && this.isPrime(g)) {
+      this.p = p;
+      this.g = g;
+    } else {
+      throw new Error("NOPEE");
+    }
+  }
+
+  isPrime(value) {
+    for (let i = 2; i < value; i++) {
+      if (value % i === 0) return false;
+    }
+    return value > 1;
+  }
+}
