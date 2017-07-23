24b83627ea34db8ad40d5d201c37bd87d10fbea6
Sam Domesjö <samdomesjo@gmail.com>
2017-07-23 19:07:11 +0100

    added a getter function to the class

--- ecmascript/sublist/sublist.js
+++ ecmascript/sublist/sublist.js
@@ -3,8 +3,12 @@ export default class List {
     this.arr = arr || [];
   }
 
+  length() {
+    return this.arr.length;
+  }
+
   compare(arr) {
-    if (this.arr.every((val, index) => val === arr[index])) return 'EQUAL';
+    if (this.arr.every((val, index) => val === arr[index]) && this.arr.length === arr.length()) return 'EQUAL';
     return 'UNEQUAL';
   }
 }
