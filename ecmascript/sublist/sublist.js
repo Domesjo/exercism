9f4c4fb1f2a3c1524765d72016e069abd0be0536
Sam Domesjö <samdomesjo@gmail.com>
2017-07-23 20:33:26 +0100

    added some more logic

--- ecmascript/sublist/sublist.js
+++ ecmascript/sublist/sublist.js
@@ -10,8 +10,14 @@ export default class List {
   compare(arr2) {
     const arr = arr2.arr;
     if (this.arr.every((val, index) => val === arr[index]) && this.arr.length === arr2.length()) return 'EQUAL';
-    else if (this.arr.every(val => arr.includes(val) || (this.arr.length === 0 && arr2.length() >= 1))) return 'SUBLIST';
     else if (arr.every(val => this.arr.includes(val) || (arr.length === 0 && this.arr.length >= 1))) return 'SUPERLIST';
+    else if (this.arr.some((val, index) => {
+      if (arr.includes(val)) {
+        const i = arr.indexOf(val);
+        return arr[i + 1] === this.arr[index + 1];
+      }
+      return false;
+    }) || (this.arr.length === 0 && arr2.length() >= 1)) return 'SUBLIST';
     return 'UNEQUAL';
   }
 }
