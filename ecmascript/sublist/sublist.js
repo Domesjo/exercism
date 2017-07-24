c6f4db97b41dcc288a78502df5e32a4bce900ade
Sam Domesjö <samdomesjo@gmail.com>
2017-07-24 22:47:06 +0100

    hosisted

--- ecmascript/sublist/sublist.js
+++ ecmascript/sublist/sublist.js
@@ -10,7 +10,6 @@ export default class List {
   compare(arr2) {
     const arr = arr2.arr;
     if (this.arr.every((val, index) => val === arr[index]) && this.arr.length === arr2.length()) return 'EQUAL';
-    else if (arr.every(val => this.arr.includes(val) || (arr.length === 0 && this.arr.length >= 1))) return 'SUPERLIST';
     else if (this.arr.some((val, index) => {
       if (arr.includes(val)) {
         const i = arr.indexOf(val); // i cannot be first index nad index can not be last
@@ -19,6 +18,14 @@ export default class List {
       }
       return false;
     }) || (this.arr.length === 0 && arr2.length() >= 1)) return 'SUBLIST';
+    else if (arr.some((val, index) => {
+      if (this.arr.includes(val)) {
+        const i = this.arr.indexOf(val);
+        const isFirstAndLast = i !== 0 && index + 1 !== (this.arr.length - 1);
+        return ((this.arr[i + 1] && arr[index + 1] && arr[index + 1] === this.arr[i + 1] && isFirstAndLast) || (arr[index + 1] === this.arr[i + 1] && arr[index + 2] && this.arr[i + 2] && arr[index + 2] === this.arr[index + 2]));
+      }
+      return false
+    }) || (arr.length === 0 && this.arr.length >= 1)) return 'SUPERLIST';
     return 'UNEQUAL';
   }
 }
