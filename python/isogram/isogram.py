151e7c0d3107d57fda817bc233fe5411a67d2a97
Sam Domesjö <samdomesjo@gmail.com>
2017-07-25 22:58:33 +0100

    passes isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -3,12 +3,11 @@ def is_isogram(word):
     word = word.lower()
     alpa = list(string.ascii_lowercase)
     for letter in word:
-        if letter != '-' or letter != ' ':
+        if letter != '-' and letter != ' ':
             print(letter)
             try:
                 alpa.remove(letter)
             except:
-                print('thorws')
                 return False
     return True
 is_isogram("Emily Jung Schwartzkopf")
