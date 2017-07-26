b3e11c9ed86c018fe85bd8eb2daf78dcc87d6858
Sam Domesjö <samdomesjo@gmail.com>
2017-07-26 14:31:37 +0100

    tries to remove x but cant find it

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,7 +2,8 @@ import string
 def is_pangram(word):
     alpa = list(string.ascii_lowercase)
     word = word.replace(' ','')
-    print(word)
     for letter in word:
-        alpa.remove(letter)
+        print(alpa)
+        alpa.remove(str(letter))
+
 is_pangram('the quick brown fox jumps over the lazy dog')
