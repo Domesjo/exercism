ba99662fef7ce6af95d38a9908d02243f8eef588
Sam Domesjö <samdomesjo@gmail.com>
2017-07-26 14:07:21 +0100

    strip whitespaces

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,4 +1,8 @@
-import 'string'
-def is_pangram():
-    print(string.ascii)
-is_pangram()
+import string
+def is_pangram(word):
+    alpa = list(string.ascii_lowercase)
+    word = word.replace(' ','')
+    print(word)
+    for letter in word:
+        alpa.remove(letter)
+is_pangram('the quick brown fox jumps over the lazy dog')
