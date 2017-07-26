b18ee41a9c0fbf7a0df40ea1e5e29299e819c848
Sam Domesjö <samdomesjo@gmail.com>
2017-07-26 14:48:38 +0100

    passes

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,9 +1,13 @@
 import string
 def is_pangram(word):
     alpa = list(string.ascii_lowercase)
-    word = word.replace(' ','')
+    word = list(word.replace(' ','').lower())
     for letter in word:
-        print(alpa)
-        alpa.remove(str(letter))
-
-is_pangram('the quick brown fox jumps over the lazy dog')
+        try:
+            i = alpa.index(letter)
+            alpa.remove(str(letter))
+        except:
+            print(' ')
+    if len(alpa) == 0:
+        return True
+    return False
