bf86b30957379d28e76e9901c1c7bbeb54e082b6
Sam Domesjö <samdomesjo@gmail.com>
2017-07-28 14:03:30 +0100

    splits with regex

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,13 +1,14 @@
+import re
 def word_count(str1):
-    str1 = str1.split()
+    str1 = re.split(r"[^A-Za-z]", str1)
     ans = {}
     for word in str1:
         try:
             if ans[word] and ans[word] >= 1:
                 ans[word] += 1
         except KeyError:
-            ans[word] = 1
-    print(ans)
+            if word != '':
+                ans[word] = 1
     return ans
 
 word_count('one of each')
