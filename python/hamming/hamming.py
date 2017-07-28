529886afea6411aa589523433ddefa0659a50859
Sam Domesjö <samdomesjo@gmail.com>
2017-07-28 11:08:28 +0100

    passes

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,6 @@
 def distance(str1, str2):
+    if len(str1) != len(str2):
+        raise ValueError("Bruuuh string not even the same length")
     diff = 0
     for i in range(len(str1)):
         if not str1[i] == str2[i]:
