9dec60e70376e8c9b898c708af169655896b1c12
Sam Domesjö <samdomesjo@gmail.com>
2017-07-28 14:06:56 +0100

    passes

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,15 +1,16 @@
 import re
 def word_count(str1):
-    str1 = re.split(r"[^A-Za-z0-9]", str1)
+    str1 = re.split(r"[^A-Za-z0-9]", str1.lower())
     ans = {}
     for word in str1:
-        print(word)
         try:
             if ans[word] and ans[word] >= 1:
                 ans[word] += 1
         except KeyError:
             if word != '':
                 ans[word] = 1
+    print(ans)
     return ans
 
-word_count('testing 1 2 testing')
+
+word_count('go Go GO Stop stop')
