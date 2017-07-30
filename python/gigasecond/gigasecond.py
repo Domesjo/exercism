010cd0648bc0c3ff761308cba680d1fd489ca303
Sam Domesjö <samdomesjo@gmail.com>
2017-07-30 23:36:05 +0100

    passes all tests

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,6 @@
-def add_gigasecond():
-    pass
+from datetime import datetime, timedelta
+def add_gigasecond(date):
+    secs=1000000000
+    return date + timedelta(seconds=secs)
+
+#add_gigasecond(datetime(2011, 4, 25))
