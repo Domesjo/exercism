243fd3f6f7f39bc098871b3155c4d99373436b16
Sam Domesjö <samdomesjo@gmail.com>
2017-08-01 15:18:13 +0100

    passes

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,9 +2,8 @@ import re
 def hey(phrase):
     if re.findall(r"[A-Z]+", phrase) and phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
         return "Whoa, chill out!"
-    elif re.match(r".*\?\s*", phrase):
+    elif re.match(r".*\?\s*$", phrase):
         return "Sure."
     elif re.findall(r"[^A-Za-z0-9]*", phrase) and not re.findall(r"[A-Za-z0-9]+", phrase):
         return "Fine. Be that way!"
     return "Whatever."
-#hey("\t\t\t\t\t\t\t\t\t\t")
