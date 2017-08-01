e3ed1d08d7762fb05135d7577e457c9fb31d5da8
Sam Domesjö <samdomesjo@gmail.com>
2017-08-01 15:11:07 +0100

    added optional regex

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,8 +1,10 @@
 import re
 def hey(phrase):
-    if re.match(r"[A-Z]", phrase) and phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
+    if re.findall(r"[A-Z]+", phrase) and phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
         return "Whoa, chill out!"
     elif re.match(r".*\?\s*", phrase):
         return "Sure."
+    elif re.findall(r"[^A-Za-z0-9]*", phrase) and not re.findall(r"[A-Za-z]+", phrase):
+        return "Fine. Be that way!"
     return "Whatever."
-hey("WHAT THE HELL WERE YOU THINKING?")
+#hey("\t\t\t\t\t\t\t\t\t\t")
