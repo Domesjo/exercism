322e585f885a4cc4d66bd4be603cdaee77b7aa0f
Sam Domesjö <samdomesjo@gmail.com>
2017-08-01 14:20:48 +0100

    progress

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,4 +2,6 @@ import re
 def hey(phrase):
     if re.match(r".*\?\s*", phrase):
         return "Sure."
+    elif phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
+        return "Whoa, chill out!"
     return "Whatever."
