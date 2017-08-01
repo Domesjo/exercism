ea605b670f9b57830b056572955c1d9e9bc1f132
Sam Domesjö <samdomesjo@gmail.com>
2017-08-01 15:13:12 +0100

    added ints

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -4,7 +4,7 @@ def hey(phrase):
         return "Whoa, chill out!"
     elif re.match(r".*\?\s*", phrase):
         return "Sure."
-    elif re.findall(r"[^A-Za-z0-9]*", phrase) and not re.findall(r"[A-Za-z]+", phrase):
+    elif re.findall(r"[^A-Za-z0-9]*", phrase) and not re.findall(r"[A-Za-z0-9]+", phrase):
         return "Fine. Be that way!"
     return "Whatever."
 #hey("\t\t\t\t\t\t\t\t\t\t")
