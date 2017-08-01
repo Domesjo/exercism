951f2fec19de853e3fbb177f0283f1488e3fe364
Sam Domesjö <samdomesjo@gmail.com>
2017-08-01 14:31:29 +0100

    progress

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,7 +1,8 @@
 import re
 def hey(phrase):
-    if re.match(r".*\?\s*", phrase):
-        return "Sure."
-    elif phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
+    if re.match(r"[A-Z]", phrase) and phrase == phrase.upper() or re.match(r"[^0-9] g", phrase) and re.match(r".*\!", phrase):
         return "Whoa, chill out!"
+    elif re.match(r".*\?\s*", phrase):
+        return "Sure."
     return "Whatever."
+hey("WHAT THE HELL WERE YOU THINKING?")
