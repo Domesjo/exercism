d1e30052c5e776db7013a351f4c65098e042fe88
Sam Domesjö <samdomesjo@gmail.com>
2017-08-04 13:34:57 +0100

    added a reduce

--- ecmascript/scrabble-score/scrabble-score.js
+++ ecmascript/scrabble-score/scrabble-score.js
@@ -1,15 +1,19 @@
 function score(word) {
-  return word.map(pickScore());
+  if(word) return word.toUpperCase().split('').reduce((sum, val) =>{
+    return sum += pickScore(val);
+  }, 0);
+  return 0;
 }
 
 function pickScore(letter) {
-  if(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].includes(letter)) return 1;
-  else if(['D', 'G' ].includes(letter)) return 2;
-  else if(['B', 'C', 'M', 'P'].includes(letter)) return 3;
-  else if(['F', 'H', 'V', 'W', 'Y'].includes(letter)) return 4;
-  else if(['K'].includes(letter)) return 5
-  else if(['J', 'X'].includes(letter)) return 8;
-  else return 10;
+  if (['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].includes(letter)) return 1;
+  else if (['D', 'G'].includes(letter)) return 2;
+  else if (['B', 'C', 'M', 'P'].includes(letter)) return 3;
+  else if (['F', 'H', 'V', 'W', 'Y'].includes(letter)) return 4;
+  else if (['K'].includes(letter)) return 5;
+  else if (['J', 'X'].includes(letter)) return 8;
+  else if (['Q', 'Z'].includes(letter)) return 10;
+  return 0;
 }
 module.exports = score;
 // A, E, I, O, U, L, N, R, S, T       1
--- ecmascript/scrabble-score/scrabble-score.spec.js
+++ ecmascript/scrabble-score/scrabble-score.spec.js
@@ -3,11 +3,11 @@ import score from './scrabble-score';
 describe('Scrabble', () => {
   test('scores an empty word as zero', () => expect(score('')).toEqual(0));
 
-  xtest('scores a null as zero', () => expect(score(null)).toEqual(0));
+  test('scores a null as zero', () => expect(score(null)).toEqual(0));
 
-  xtest('scores a very short word', () => expect(score('a')).toEqual(1));
+  test('scores a very short word', () => expect(score('a')).toEqual(1));
 
-  xtest('scores the word by the number of letters', () => expect(score('street')).toEqual(6));
+  test('scores the word by the number of letters', () => expect(score('street')).toEqual(6));
 
   xtest('scores more complicated words with more', () => expect(score('quirky')).toEqual(22));
 
