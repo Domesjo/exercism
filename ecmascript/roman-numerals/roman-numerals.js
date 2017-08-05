65ce30d969c2ff6082bcaeee7a064167afed2966
Sam Domesjö <samdomesjo@gmail.com>
2017-08-05 11:47:49 +0100

    hardeddd regex lolz

--- ecmascript/roman-numerals/roman-numerals.js
+++ ecmascript/roman-numerals/roman-numerals.js
@@ -12,7 +12,6 @@ function toRoman(num) {
   };
   const numbers = [1000, 500, 100, 50, 10, 5, 1];
   numbers.forEach((number, index) => {
-  //  const nextNumber = numbers[index + 1] || 1;
     while (int > 0) {
       if (int - (number - 1) >= 0) {
         if (int - number >= 0) {
@@ -25,10 +24,14 @@ function toRoman(num) {
       } else {
         break;
       }
-      if(ans.match(/(.)\1\1\1/)) ans = ans.replace(/(.)\1\1\1/, `${inRoman[number]}${inRoman[numbers[index - 1]]}`);
+      if (ans.match(/(.)\1\1\1/)) ans = ans.replace(/(.)\1\1\1/, `${inRoman[number]}${inRoman[numbers[index - 1]]}`);
+      if (ans.match(/LXL/)) ans = ans.replace(/LXL/, 'XC');
+      if (ans.match(/DCD/)) ans = ans.replace(/DCD/, 'CM');
     }
   });
   return ans;
 }
+
+
 toRoman(93);
 module.exports = toRoman;
