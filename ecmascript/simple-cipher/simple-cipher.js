a9b6d58390ff9de726d3fb096e7976cc63cd3d77
Sam Domesjö <samdomesjo@gmail.com>
2017-08-10 18:23:32 +0100

    made afunction to see if i can crack the encode

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -2,7 +2,6 @@ class Cipher {
   constructor(key = this.generateRandomKey()) {
     if (key.match(/^[a-z]+$/)) {
       this.key = key;
-      this.alpa = 'aqzwsxedcrfvtgbyhnujmikolp';
       this.cipher = '';
     } else {
       throw Error('Bad key');
@@ -11,7 +10,7 @@ class Cipher {
 
   encode(str) {
      this.key = str;
-    return this.key;
+    return str;
   }
 
   decode(str) {
@@ -20,6 +19,7 @@ class Cipher {
 
   generateRandomKey() {
     let key = '';
+    this.alpa = 'aqzwsxedcrfvtgbyhnujmikolp'.split('').sort().join('');
     for (let i = 0; i < 100; i++) {
       key += this.alpa[Math.floor(Math.random() * 26)];
     }
@@ -29,3 +29,15 @@ class Cipher {
 }
 
 export default Cipher;
+
+function encoded(str) {
+  const alpa = 'aqzwsxedcrfvtgbyhnujmikolp'.split('').sort();
+  const cipher = 'dabcefghijklmnopqrstuvwxyz';
+  const obj = {};
+  alpa.forEach((val, i) => {
+    obj[val] = cipher[i];
+  });
+  const ans = str.split('').map(letter => letter = obj[letter]).join('');
+  console.log(ans);
+}
+encoded('iamapandabear');
