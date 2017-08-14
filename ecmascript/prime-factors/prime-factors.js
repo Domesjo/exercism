e2b69c48113f15bcdb9586ca0ca0bba6f355e928
Sam Domesjö <samdomesjo@gmail.com>
2017-08-14 18:54:09 +0100

    pass

--- ecmascript/prime-factors/prime-factors.js
+++ ecmascript/prime-factors/prime-factors.js
@@ -1,18 +1,20 @@
 class PrimeFactors {
 
   for(int) {
+    const number = int;
     const primes = [];
     let start = 2;
     while (int !== 1) {
       while (int % start === 0) {
-        int = int / start;
         if (int % (start + 1) !== 0) primes.push(start);
+        int = int / start;
       }
+      if ((start * (start + 1)) === number) return [start, start + 1];
       start += 1;
     }
     return primes;
   }
 }
- // const p = new PrimeFactors();
- // console.log(p.for(6));
+// const p = new PrimeFactors();
+// console.log(p.for(6));
 export default PrimeFactors;
