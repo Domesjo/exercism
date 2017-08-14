87c459b2b72389d697aed6d28acf00b4121d5f3e
Sam Domesjö <samdomesjo@gmail.com>
2017-08-14 17:14:33 +0100

    binary completed

--- ecmascript/binary/binary.js
+++ ecmascript/binary/binary.js
@@ -4,7 +4,7 @@ class Binary {
   }
 
   toDecimal() {
-    if (this.decimal.match(/^0+[a-z]*|[a-z]/g)) return 0;
+    if (!this.decimal.match(/^[1-9][0-9]*$|^00+[0-9]*/g)) return 0;
 
     const ans = this.decimal.split('').map((number, i) => {
       return parseInt(number) * Math.pow(2, (this.decimal.length - (i + 1)));
--- ecmascript/binary/binary.spec.js
+++ ecmascript/binary/binary.spec.js
@@ -9,17 +9,17 @@ describe('binary', () => {
 
   test('11 is decimal 3', () => expect(new Binary('11').toDecimal()).toEqual(3));
 
-  xtest('100 is decimal 4', () => expect(new Binary('100').toDecimal()).toEqual(4));
+  test('100 is decimal 4', () => expect(new Binary('100').toDecimal()).toEqual(4));
 
-  xtest('1001 is decimal 9', () => expect(new Binary('1001').toDecimal()).toEqual(9));
+  test('1001 is decimal 9', () => expect(new Binary('1001').toDecimal()).toEqual(9));
 
-  xtest('11010 is decimal 26', () => expect(new Binary('11010').toDecimal()).toEqual(26));
+  test('11010 is decimal 26', () => expect(new Binary('11010').toDecimal()).toEqual(26));
 
-  xtest('10001101000 is decimal 1128', () => expect(new Binary('10001101000').toDecimal()).toEqual(1128));
+  test('10001101000 is decimal 1128', () => expect(new Binary('10001101000').toDecimal()).toEqual(1128));
 
-  xtest('00011111 is decimal 31', () => expect(new Binary('00011111').toDecimal()).toEqual(31));
+  test('00011111 is decimal 31', () => expect(new Binary('00011111').toDecimal()).toEqual(31));
 
-  xtest('invalid inputs are decimal 0', () => {
+  test('invalid inputs are decimal 0', () => {
     expect(new Binary('carrot').toDecimal()).toEqual(0);
     expect(new Binary('012').toDecimal()).toEqual(0);
     expect(new Binary('10nope').toDecimal()).toEqual(0);
