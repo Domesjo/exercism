f07edfaec6cca8f95881af2a2b1712ea64a38f72
Sam Domesjö <samdomesjo@gmail.com>
2017-08-25 15:57:59 +0100

    creates the first row

--- ecmascript/pascals-triangle/pascals-triangle.js
+++ ecmascript/pascals-triangle/pascals-triangle.js
@@ -1,6 +1,14 @@
 class Triangle{
-  constructor(input) {
+  constructor(rowsNumber) {
+    this.amountOfRows = rowsNumber; // sets the number of Rows
+    this.rows = [];
+    this.createTriangle();
+  }
 
+  createTriangle() {
+    for (let i = 0; i < this.amountOfRows; i++) {
+      if (this.rows.length === 0) this.rows.push([1]); // if no rows created push first
+    }
   }
 }
 
