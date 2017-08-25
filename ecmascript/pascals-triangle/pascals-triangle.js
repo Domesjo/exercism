bbac222a8e1dd1ef35d95c44d4f033fd00873f18
Sam Domesjö <samdomesjo@gmail.com>
2017-08-25 16:11:01 +0100

    forgot to push the next row

--- ecmascript/pascals-triangle/pascals-triangle.js
+++ ecmascript/pascals-triangle/pascals-triangle.js
@@ -6,8 +6,17 @@ class Triangle{
   }
 
   createTriangle() {
-    for (let i = 0; i < this.amountOfRows; i++) {
-      if (this.rows.length === 0) this.rows.push([1]); // if no rows created push first
+    for (let i = 0; i < this.amountOfRows; i++) { // i is what row it is starting from the top
+      if (i === 0) { // if there are no rows
+        this.rows.push([1]); // if no rows created push first
+      } else {
+        // takes the previous row to know how many intergers there should be in the next row
+        const nextRow = [];
+        for (let x = 0; x <= this.rows[i - 1].length; x++) {
+          nextRow.push(this.rows[i + 1] + this.rows[i]);
+        }
+        this.rows.push(nextRow);
+      }
     }
   }
 }
