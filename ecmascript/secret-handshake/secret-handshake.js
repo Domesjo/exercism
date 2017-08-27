b657f135ea38eeda64d8b243f55b8306e38b0cee
Sam Domesjö <samdomesjo@gmail.com>
2017-08-27 16:34:24 +0100

    a for loop in a while loop

--- ecmascript/secret-handshake/secret-handshake.js
+++ ecmascript/secret-handshake/secret-handshake.js
@@ -1,10 +1,27 @@
 class SecretHandshake {
   constructor(hexaDecimal) {
-    this.binary = hexaDecimal.toString(2);
+    this.binary = parseInt(hexaDecimal.toString(2)); // converts the hexadecimal to binary
+
+    this.handShakes = { // all the different type of handshakes
+      1: 'wink',
+      10: 'double blink',
+      100: 'close your eyes',
+      1000: 'jump',
+    };
+    this.handShake = []; // the secret handshake
   }
 
   commands() {
-    return this.binary;
+    const keys = Object.keys(this.handShakes).reverse(); // returns the keys from objects so I can loop through
+    while (this.binary > 0) {
+      keys.forEach((key) => {
+        if (this.binary - parseInt(key) >= 0) {
+          this.handShake.push(this.handShakes[key]);
+          this.binary -= parseInt(key);
+        }
+      });
+    }
+    return this.handShake;
   }
 }
 
