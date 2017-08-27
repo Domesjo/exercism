3eb46122d751714b1a36429c2d710cc3923e3d61
Sam Domesjö <samdomesjo@gmail.com>
2017-08-27 17:47:46 +0100

    refactored

--- ecmascript/secret-handshake/secret-handshake.js
+++ ecmascript/secret-handshake/secret-handshake.js
@@ -14,26 +14,19 @@ class SecretHandshake {
 
   commands() {
     // if its an even number it shall reverse it otherwise it needs to count backwards
-    const keys = this.binary % 2 === 0 ? Object.keys(this.handShakes).reverse() : Object.keys(this.handShakes); // returns the keys from objects so I can loop through
+    const keys = this.binary % 2 === 0 ? Object.keys(this.handShakes).reverse() : Object.keys(this.handShakes);
+    const bigger = this.binary > 10000;
+     // returns the keys from objects so I can loop through
+
     while (this.binary > 0) {
-      if (this.binary < 10000) {
+      if (this.binary > 10000) this.binary -= 10000;
         keys.forEach((key) => {
           if (this.binary - parseInt(key) >= 0) {
             this.handShake.push(this.handShakes[key]);
             this.binary -= parseInt(key);
           }
         });
-        return this.handShake
-      } else {
-        this.binary -= 10000;
-        keys.forEach((key) => {
-          if (this.binary - parseInt(key) >= 0) {
-            this.handShake.push(this.handShakes[key]);
-            this.binary -= parseInt(key);
-          }
-        });
-        return this.handShake.reverse();
-      }
+        return bigger ? this.handShake.reverse() : this.handShake;
     }
   }
 }
