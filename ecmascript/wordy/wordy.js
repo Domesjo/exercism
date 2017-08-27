c83e816639e775b04d02339a01b53288ca3ea413
Sam Domesjö <samdomesjo@gmail.com>
2017-08-27 18:23:47 +0100

    passes

---
+++ ecmascript/wordy/wordy.js
@@ -0,0 +1,23 @@
+class WordProblem {
+  constructor(question) { // sets the question
+    this.question = question;
+    this.operator = {
+      plus: '+',
+      minus: '-',
+    };
+  }
+
+  answer() {
+    let ans = '';
+    this.question.match(/[0-9]|plus|minus/g).forEach((val) => {
+      if (this.operator[val]) {
+        ans += this.operator[val];
+      } else {
+        ans += val;
+      }
+    });
+    return eval(ans);
+  }
+}
+
+module.exports = { WordProblem };
