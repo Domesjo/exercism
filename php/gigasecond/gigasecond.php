768df847b167c8c01eced444cd57235301a3eba1
Sam Domesjö <samdomesjo@gmail.com>
2017-08-29 22:22:32 +0100

    mange to turn it into time

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -1,9 +1,9 @@
+use \DateTime;
 <?php
 
 function from($date)
 {
   $billion = pow(10, 9);
-  fputs(STDOUT, strtotime("$billion sec") . "\n"); // logs to the terminal
-  return $date->add($billion);
-  return date($date, strtotime("+$billion sec"));
+  fputs(STDOUT, date("Y-m-d H:i:s", $billion) . "\n----------\n"); // logs to the terminal
+  return $date;
 }
