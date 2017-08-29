3df2e1dd5c739a16ac736ba38c6b49c3e4c12e25
Sam Domesjö <samdomesjo@gmail.com>
2017-08-29 21:54:03 +0100

    learn to ooutput data

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -3,5 +3,7 @@
 function from($date)
 {
   $billion = pow(10, 9);
+  fputs(STDOUT, strtotime("$billion sec") . "\n"); // logs to the terminal
+  return $date->add($billion);
   return date($date, strtotime("+$billion sec"));
 }
