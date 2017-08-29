9bb3015aba1bf98d927dffc79da2a0d8586af972
Sam Domesjö <samdomesjo@gmail.com>
2017-08-29 22:48:43 +0100

    finally passes

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -4,7 +4,5 @@ use \DateTime;
 function from($date)
 {
   $billion = pow(10, 9);
-  fputs(STDOUT, date_timestamp_get($date));
-  fputs(STDOUT, gmdate('H:i', $billion) . "\n----------\n"); // logs to the terminal
-  return $date;
+  return $date->add(new DateInterval("PT{$billion}S"));
 }
