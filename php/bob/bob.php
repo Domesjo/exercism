b6edf27c33f3bd967e4837bbd7d6e86f9b1187c2
Sam Domesjö <samdomesjo@gmail.com>
2017-09-12 23:01:28 +0100

    passes all bob testS

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -4,12 +4,15 @@ class Bob {
 
   public function respondTo($phrase)
   {
-    if(preg_match_all("/[A-Z]+$/", $phrase) || $phrase === strtoupper($phrase))
+    if(preg_match_all("/[A-Z]+$/", $phrase) || ($phrase === strtoupper($phrase) && preg_match("/[A-Za-z]/", $phrase)))
     {
       return "Whoa, chill out!";
-    } else if(preg_match("/.+\?$/", $phrase))
+    } else if(preg_match("/.+\?$/", $phrase) || preg_match("/.+\?\s+$/", $phrase))
     {
       return "Sure.";
+    } else if(preg_match("/^\s+$/", $phrase) || !$phrase)
+    {
+      return "Fine. Be that way!";
     }
 
     return "Whatever.";
