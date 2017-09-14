07fa78fe5e5cd51c4442eba2ddf0fb221e025204
Sam Domesjö <samdomesjo@gmail.com>
2017-09-14 22:45:02 +0100

    passes

--- php/pangram/pangram.php
+++ php/pangram/pangram.php
@@ -4,5 +4,11 @@ function isPangram($phrase)
 {
   $alpa = range('a', 'z'); // gets the length of alpabeth
   $phrase = str_split($phrase);
-  var_dump($phrase);
+
+  foreach($phrase as $letter)
+  {
+    var_dump($letter);
+  }
+
+  return sizeof($alpa) == 0 ? true: false;
 }
