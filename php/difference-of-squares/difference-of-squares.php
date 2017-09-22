0b807ee97b2b21a21967282714d0994689eb12fe
Sam Domesjö <samdomesjo@gmail.com>
2017-09-22 23:45:24 +0100

    sorted

--- php/difference-of-squares/difference-of-squares.php
+++ php/difference-of-squares/difference-of-squares.php
@@ -2,5 +2,25 @@
 
 function squareOfSums(int $sqr)
 {
-  var_dump($sqr);
+  return addedSum($sqr, '+') * addedSum($sqr, '+');
+
+}
+
+function addedSum(int $n, $operator)
+{
+    $sum = 0;
+    for ($i = 0 ; $i <= $n ;$i++)
+    {
+        if($operator == '+')
+        {
+            $sum = $sum + $i;
+        } else if ($operator == '^')
+        {
+            $sum = pow($i, 2);
+        } else {
+            throw new Error("Inavlid operator");
+        }
+    }
+    var_dump($sum);
+    return $sum;
 }
